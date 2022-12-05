be2937b716e4eab9defff21d564f9c91b6c3e92c
Dat0309 <anhtheboy011@gmail.com>
2022-12-05 09:00:52 +0700

    update

--- Routes/BookingRouter.js
+++ Routes/BookingRouter.js
@@ -147,12 +147,14 @@ bookingRouter.get(
     const date = req.body.date;
     var tables = [];
 
-    await Booking.find({ date: "30/09/2022" }).then((result) => {
-      result.forEach(async (e) => {
-        var table = await Table.findById(e.table_id);
-        tables.push(table);
-      });
-    });
+    const bookings = await Booking.find({ date: "30/09/2022" }).then(
+      (result) => {
+        result.forEach(async (e) => {
+          var table = await Table.findById(e.table_id);
+          tables.push(table);
+        });
+      }
+    );
 
     res.json(tables);
   })
